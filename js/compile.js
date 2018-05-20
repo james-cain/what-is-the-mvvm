acc3fbbd967afdaf1255da3790aa633ad2cfcf13
james-cain <m15960192140@163.com>
2018-05-21 03:25:04 +0800

    feat: add demo

--- js/compile.js
+++ js/compile.js
@@ -1,6 +1,7 @@
 // compile主要做的事情是解析模板指令，将模板中的变量替换成数据，然后初始化渲染页面视图
 // 并将每个指令对应的节点绑定更新函数，添加监听数据的订阅者，一旦数据有变动，收到通知，更新视图
-function Compile(el) {
+function Compile(el, vm) {
+    this.$vm = vm;
     this.$el = this.isElementNode(el) ? el : document.querySelector(el);
     if (this.$el) {
         this.$fragment = this.node2Fragment(this.$el);
