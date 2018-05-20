f844ee896aa36833baeb0ac53c88a73b0ecfa42d
james-cain <m15960192140@163.com>
2018-05-20 17:21:27 +0800

    添加属性代理

--- js/mvvm.js
+++ js/mvvm.js
@@ -3,7 +3,27 @@
 // 视图交互变化（input）->数据model变更的双向绑定效果
 function MVVM(options) {
     this.$options = options;
-    var data = this._data = this.$options.data;
+    var data = this._data = this.$options.data, me = this;
+    // 添加属性代理
+    Object.keys(data).forEach(function(key) {
+        me._proxy(key);
+    });
     observe(data, this);
     this.$compile = new Compile(options.el || document.body, this);
 }
+
+MVVM.prototype = {
+    _proxy: function(key) {
+        var me = this;
+        Object.defineProperty(me, key, {
+            enumerable: true,
+            configurable: false,
+            get: function proxyGetter() {
+                return me._data[key];
+            },
+            set: function proxySetter(newVal) {
+                me._data[key] = newVal;
+            }
+        });
+    }
+};
